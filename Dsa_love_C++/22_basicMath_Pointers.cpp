69a55ad76a7af4fef1c6e18013a8094b79ea007f
Hemanta <hemantabhoi16112003@gmail.com>
2024-07-25 23:34:20 +0530

    basic math pointers

--- Dsa_love_C++/22_basicMath_Pointers.cpp
+++ Dsa_love_C++/22_basicMath_Pointers.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace std;
 
+class Solution {
+public:
+    // bool isPrime(int n){
+    //     if(n <= 1){
+    //         return false;
+    //     }
+    //     for(int i = 2; i <= sqrt(n); i++){
+    //         if(n % i == 0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool isPrime(int n){
+        if(n <= 1){
+            return false;
+        }
+        for(int i = 2; i <= sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    int countPrimes(int n) {
+        int totalPrimeNumbers = 0;
+        for(int i = 0; i < n; i++){
+            if(isPrime(i)){
+                totalPrimeNumbers++;
+            }
+        }
+        return totalPrimeNumbers;
+    }
+};
 
 
 int main(){
