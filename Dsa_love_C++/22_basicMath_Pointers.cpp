67c9985ce0ee44bd96393933872a4d1e2dc8aa5c
Hemanta <hemantabhoi16112003@gmail.com>
2024-07-26 01:09:52 +0530

    updated

--- Dsa_love_C++/22_basicMath_Pointers.cpp
+++ Dsa_love_C++/22_basicMath_Pointers.cpp
@@ -5,17 +5,6 @@ using namespace std;
 
 class Solution {
 public:
-    // bool isPrime(int n){
-    //     if(n <= 1){
-    //         return false;
-    //     }
-    //     for(int i = 2; i <= sqrt(n); i++){
-    //         if(n % i == 0){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
     bool isPrime(int n){
         if(n <= 1){
             return false;
@@ -28,13 +17,33 @@ public:
         return true;
     }
     int countPrimes(int n) {
-        int totalPrimeNumbers = 0;
-        for(int i = 0; i < n; i++){
-            if(isPrime(i)){
-                totalPrimeNumbers++;
+        // int totalPrimeNumbers = 0;
+        // for(int i = 0; i < n; i++){
+        //     if(isPrime(i)){
+        //         totalPrimeNumbers++;
+        //     }
+        // }
+        if(n == 0) return 0;
+
+        vector<bool> prime(n, true);
+        prime[0] = prime[1] = false;
+
+        int ans = 0;
+
+        for(int i = 2; i < n; i++){
+            if(prime[i]){
+                ans++;
+
+                int j = 2 * i;
+                while(j < n){
+                    prime[j] = true;
+                    j += i;
+                }
             }
         }
-        return totalPrimeNumbers;
+
+        return ans;
+        // return totalPrimeNumbers;
     }
 };
 
