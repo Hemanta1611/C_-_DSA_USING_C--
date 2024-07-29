0b7999d6e74fa7022659aa027f766a21eccfebf3
Hemanta <hemantabhoi16112003@gmail.com>
2024-07-29 23:13:18 +0530

    summary of pointers

--- Dsa_love_C++/23_Pointers.cpp
+++ Dsa_love_C++/23_Pointers.cpp
@@ -18,3 +18,13 @@ int main(){
     
     return 0;
 }
+
+/*
+Summary
+Declaration: int *ptr; declares a pointer to an integer.
+Assignment: ptr = &var; assigns the address of var to ptr.
+Dereferencing: *ptr accesses the value at the address stored in ptr.
+Dynamic Memory Allocation: int *ptr = new int; allocates memory and delete ptr; deallocates it.
+Array Access: Pointers can be used to access array elements.
+Pointer to Pointer: int **pptr = &ptr; allows a pointer to another pointer.
+*/
