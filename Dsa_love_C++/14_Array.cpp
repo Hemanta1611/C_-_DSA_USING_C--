606e6c2745d8387e8da23ec62e1ca7778f9f7142
Hemanta Kumar Bhoi <hemantabhoi16112003@gmail.com>
2024-12-11 17:07:58 +0530

    dynamic memory allocation without new keyword

--- Dsa_love_C++/14_Array.cpp
+++ Dsa_love_C++/14_Array.cpp
@@ -20,6 +20,43 @@ int main(){
     }
     */
 
+    // Dynamic Memory Allocation:
+    // 1. allocate int
+    int *intPtr = new int(5); // pointer variable 'intPtr' that stores the value '5' in heap memory
+    // without using new keyword
+    int *intPtr2 = (int *) malloc(sizeof(int));
+    *intPtr2 = 5;
+
+    delete intPtr;
+    free(intPtr2);
+
+
+    // 2. allocate array
+    int *arr = new int[5];
+    // without using new keyword
+    int *arr2 = (int *) malloc(5*sizeof(int));
+
+    delete [] arr;
+    free(arr2);
+
+    // 3. allocate 2D array
+    int rows = 5, cols = 5;
+    int **arr3 = new int*[rows];
+    for(int i = 0; i<rows; i++){
+        arr3[i] = new int[cols];
+    }
+    // without using new keyword
+    int **arr4 = (int **) malloc(rows*sizeof(int*));
+    for(int i = 0; i<rows; i++){
+        arr4[i] = (int *) malloc(cols*sizeof(int));
+    }
+    
+    for(int i = 0; i<rows; i++){
+        free(arr4[i]);
+        delete [] arr3[i];
+    }
+    delete [] arr3;
+    free(arr4);
     
     
     
